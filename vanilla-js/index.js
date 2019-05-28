6cb3085ad6ac4b1918172ae0f44cc50b006e2f9e
Jeremyegan <jeremyegan1@gmail.com>
2019-05-28 16:10:57 -0600

    update

---
+++ vanilla-js/index.js
@@ -0,0 +1,10 @@
+console.log('Hello from index.js')
+const heading = document.getElementById('heading')
+console.log(heading)
+// heading.innerText = "I don't know"
+// heading.style.background = "blue"
+// heading.style.color = "#ff0099"
+// heading.style.height = "500px"
+const headingTags = document.getElementsByTagName('h1')
+console.log(headingTags)
+const box = document.getElementById('box')
